364a4273557674f439ab3babf214f9c3c6e2f1ad
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-09-17 09:35:36 -0400

    Added JWT dependencies for user authentification

---
+++ BackEnd/src/main/java/com/bigappleportal/utils/Utils.java
@@ -0,0 +1,140 @@
+package com.bigappleportal.utils;
+
+import com.bigappleportal.dto.ApprenticeshipDTO;
+import com.bigappleportal.dto.ApprenticeshipCategoryDTO;
+import com.bigappleportal.dto.ApplicationDTO;
+import com.bigappleportal.dto.UserDTO;
+import com.bigappleportal.exceptions.OurException;
+import com.bigappleportal.model.Apprenticeship;
+import com.bigappleportal.model.ApprenticeshipCategory;
+import com.bigappleportal.model.Application;
+import com.bigappleportal.model.User;
+
+import java.security.SecureRandom;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Utils {
+
+    private static final String ALPHANUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+
+    // Map User entity to UserDTO
+    public static UserDTO mapUserEntityToUserDTO(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setName(user.getName());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPhoneNumber(user.getPhoneNumber());
+        userDTO.setRole(user.getRole());
+//        userDTO.setProfile(user.getProfile());
+        return userDTO;
+    }
+
+    // Map Apprenticeship entity to ApprenticeshipDTO
+    public static ApprenticeshipDTO mapApprenticeshipEntityToApprenticeshipDTO(Apprenticeship apprenticeship) {
+        ApprenticeshipDTO apprenticeshipDTO = new ApprenticeshipDTO();
+        apprenticeshipDTO.setId(apprenticeship.getId());
+        apprenticeshipDTO.setTitle(apprenticeship.getTitle());
+        apprenticeshipDTO.setDescription(apprenticeship.getDescription());
+        apprenticeshipDTO.setLocation(apprenticeship.getLocation());
+        apprenticeshipDTO.setStatus(apprenticeship.getStatus());
+        apprenticeshipDTO.setDatePosted(apprenticeship.getDatePosted());
+
+        return apprenticeshipDTO;
+    }
+
+    // Map Application entity to ApplicationDTO
+    public static ApplicationDTO mapApplicationEntityToApplicationDTO(Application application) {
+        ApplicationDTO applicationDTO = new ApplicationDTO();
+        applicationDTO.setId(application.getId());
+        applicationDTO.setDateApplied(application.getDateApplied());
+        applicationDTO.setStatus(application.getStatus());
+        applicationDTO.setUser(mapUserEntityToUserDTO(application.getUser()));
+        applicationDTO.setApprenticeship(mapApprenticeshipEntityToApprenticeshipDTO(application.getApprenticeship()));
+        return applicationDTO;
+    }
+
+    // Map ApprenticeshipCategory entity to ApprenticeshipCategoryDTO
+    public static ApprenticeshipCategoryDTO mapApprenticeshipCategoryEntityToApprenticeshipCategoryDTO(ApprenticeshipCategory category) {
+        ApprenticeshipCategoryDTO categoryDTO = new ApprenticeshipCategoryDTO();
+        categoryDTO.setId(category.getId());
+        categoryDTO.setName(category.getName());
+        categoryDTO.setDescription(category.getDescription());
+        return categoryDTO;
+    }
+
+    // Map User entity to UserDTO, including associated applications
+    public static UserDTO mapUserEntityToUserDTOWithApplications(User user) {
+        UserDTO userDTO = mapUserEntityToUserDTO(user);
+        if (!user.getApplications().isEmpty()) {
+            userDTO.setApplications(user.getApplications().stream()
+                    .map(Utils::mapApplicationEntityToApplicationDTO)
+                    .collect(Collectors.toList()));
+        }
+        return userDTO;
+    }
+
+
+
+    // Map Apprenticeship entity to ApprenticeshipDTO, including associated applications
+    public static ApprenticeshipDTO mapApprenticeshipEntityToApprenticeshipDTOWithApplications(Apprenticeship apprenticeship) {
+        ApprenticeshipDTO apprenticeshipDTO = mapApprenticeshipEntityToApprenticeshipDTO(apprenticeship);
+        if (apprenticeship.getApplications() != null && !apprenticeship.getApplications().isEmpty()) {
+            apprenticeshipDTO.setApplications(apprenticeship.getApplications().stream()
+                    .map(Utils::mapApplicationEntityToApplicationDTO)
+                    .collect(Collectors.toList()));
+        }
+        return apprenticeshipDTO;
+    }
+
+    // Map a list of User entities to a list of UserDTOs
+    public static List<UserDTO> mapUserListEntityToUserListDTO(List<User> userList) {
+        return userList.stream().map(Utils::mapUserEntityToUserDTO).collect(Collectors.toList());
+    }
+
+    // Map a list of Apprenticeship entities to a list of ApprenticeshipDTOs
+    public static List<ApprenticeshipDTO> mapApprenticeshipListEntityToApprenticeshipListDTO(List<Apprenticeship> apprenticeshipList) {
+        return apprenticeshipList.stream().map(Utils::mapApprenticeshipEntityToApprenticeshipDTO).collect(Collectors.toList());
+    }
+
+
+
+    // Map a list of Application entities to a list of ApplicationDTOs
+    public static List<ApplicationDTO> mapApplicationListEntityToApplicationListDTO(List<Application> applicationList) {
+        return applicationList.stream().map(Utils::mapApplicationEntityToApplicationDTO).collect(Collectors.toList());
+    }
+
+    // Map a list of ApprenticeshipCategory entities to a list of ApprenticeshipCategoryDTOs
+    public static List<ApprenticeshipCategoryDTO> mapApprenticeshipCategoryListEntityToApprenticeshipCategoryListDTO(List<ApprenticeshipCategory> categoryList) {
+        return categoryList.stream().map(Utils::mapApprenticeshipCategoryEntityToApprenticeshipCategoryDTO).collect(Collectors.toList());
+    }
+
+
+
+    public static Apprenticeship mapApprenticeshipDTOToApprenticeshipEntity(ApprenticeshipDTO apprenticeshipDTO) {
+        Apprenticeship apprenticeship = new Apprenticeship();
+
+        apprenticeship.setTitle(apprenticeshipDTO.getTitle());
+        apprenticeship.setLocation(apprenticeshipDTO.getLocation());
+        apprenticeship.setDescription(apprenticeshipDTO.getDescription());
+        apprenticeship.setApprenticeshipType(apprenticeshipDTO.getApprenticeshipType());
+        apprenticeship.setSalaryRange(apprenticeshipDTO.getSalaryRange());
+        apprenticeship.setExperienceLevel(apprenticeshipDTO.getExperienceLevel());
+        apprenticeship.setRequiredSkills(apprenticeshipDTO.getRequiredSkills());
+        apprenticeship.setStatus(apprenticeshipDTO.getStatus());
+        apprenticeship.setDatePosted(apprenticeshipDTO.getDatePosted());
+
+        return apprenticeship;
+    }
+
+
+
+}
+
+
+
+
+
---
+++ BackEnd/src/test/java/com/bigappleportal/BigApplePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigappleportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigApplePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
